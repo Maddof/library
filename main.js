229db426aca1dfc1f8381a8c76ab989ea680dec0
Maddof <majd.azar88@gmail.com>
2024-01-17 14:35:38 +0100

    Big update

--- main.js
+++ main.js
@@ -1,16 +1,65 @@
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+const myLibrary = [
+  {
+    title: "The Hobbit",
+    author: "J.R.R Tolkien",
+    status: "not read",
+  },
+  {
+    title: "When the Moon Hatched",
+    author: "Sarah A. Parker",
+    status: "read",
+  },
+  {
+    title: "The Book of Bill",
+    author: "Alex Hirsch",
+    status: "Not read",
+  },
+];
+
+// function Book(title, author, status) {
+//   this.title = title;
+//   this.author = author;
+//   this.status = status;
+// }
+
+// Book.prototype.info = function () {
+//   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`;
+// };
+
+function addBookToLibrary(newBookObj) {
+  myLibrary.push(newBookObj);
+  displayBooks();
 }
-Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-};
 
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read yet");
-const starwars = new Book("Star Wars", "Majd Azar", 201, "read");
+function displayBooks() {
+  const bookList = document.querySelector(".books");
+  bookList.innerHTML = "";
+  myLibrary.forEach((book, i) => {
+    const htmlBook = `
+        <li class ="books-item" index="${i}">
+            <div>${book.title}</div>
+            <div>${book.author}</div>
+            <div>${book.status}</div>
+            <div><button class="delete" index="${i}" onClick="deleteBook(${i})">Delete</button></div>
+        </li>
+    `;
+    bookList.insertAdjacentHTML("beforeend", htmlBook);
+  });
+}
 
-console.log(theHobbit.info());
-console.log(starwars.info());
-console.log(starwars);
+displayBooks();
+
+const form = document.querySelector(".book-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let formData = new FormData(form);
+  // output as an object
+  addBookToLibrary(Object.fromEntries(formData));
+  // clear inputs
+  form.reset();
+});
+
+function deleteBook(i) {
+  myLibrary.splice(i, 1);
+  displayBooks();
+}
